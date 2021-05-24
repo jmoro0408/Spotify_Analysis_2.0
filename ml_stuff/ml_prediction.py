d8854443a18cb467feea9bb5f8e29ec65cdd24d3
James Moro <james.moro.11@gmail.com>
2021-05-24 09:35:37 -0700

    added random smapling checks of track IDs

--- ml_stuff/ml_prediction.py
+++ ml_stuff/ml_prediction.py
@@ -7,6 +7,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow import keras
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
 
 
 def convert_duration(dataframe):
@@ -35,10 +37,27 @@ columns_to_drop = [
 stones_features.drop(columns_to_drop, inplace=True, axis=1)
 stones_features = convert_duration(stones_features)
 
-X = stones_features.copy()
-x_test = X.iloc[0].to_numpy()
-
 trained_model_dir = r"/Users/James/Documents/Python/Machine Learning Projects/Spotify_Listening_Analysis/Spotify 2.0/ml_stuff/trained_model.h5"
 model = keras.models.load_model(trained_model_dir)
+# model.summary()
 
-print(model.predict(x_test))
+X = stones_features.copy()
+standard_scalar = StandardScaler()
+X = standard_scalar.fit_transform(X)
+
+predictions = {"artist": [], "track": [], "predicted_playcount": []}
+for count, track in enumerate(X):
+    predictions["artist"].append(stones_features_raw["artistName"].iloc[count])
+    predictions["track"].append(stones_features_raw["trackName"].iloc[count])
+    track = X[count].reshape(1, 12)
+    predictions["predicted_playcount"].append(model.predict(track))
+    if count % 100 == 0:
+        print(f"{count} of {len(X)}")
+
+predictions_df = pd.DataFrame(predictions)
+predictions_df["predicted_playcount"] = predictions_df["predicted_playcount"].astype(
+    float
+)
+current_directory = os.getcwd()
+pickle_name = "prediction_dataframe.pkl"
+predictions_df.to_pickle(os.path.join(current_directory, pickle_name))
